f9b29587cd7baf191be7c81f6c4a157e7ac63eb0
mauriani <maurianim@gmail.com>
2023-02-02 16:58:00 -0300

    adicionando tabs navigator

--- src/routes/user.stack.routes.tsx
+++ src/routes/user.stack.routes.tsx
@@ -4,16 +4,12 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { Home } from "../screens/Home";
 import { Product } from "../screens/Product";
-import { Order } from "../screens/Order";
-import { Orders } from "../screens/Orders";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
 export function UserStackRoutes() {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
-      <Screen name="orders" component={Orders} />
-      <Screen name="order" component={Order} />
       <Screen name="home" component={Home} />
       <Screen name="product" component={Product} />
     </Navigator>
